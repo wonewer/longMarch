73fb922ac55d902d10c57b6890793b9e1934e981
yuanliangchen <wonewer@163.com>
2017-11-20 11:31:33 +0800

    Update Kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -34,12 +34,22 @@ class Kernel
     {
         $callback = function ($message){
             try {
-                $json = $message->body;
+                $json = preg_replace('/[\s\0]+/','',$message->body);
                 $data = json_decode($json);
                 $arrayiter = new JsonIter($data);
                 new Aop($arrayiter,$this->dbConf);
                 $message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']);
             } catch (\Exception $e) {
+                $info = PHP_EOL . 'start=======' . date('Y-m-d H:i:s') . '=========' . PHP_EOL;
+                $info .= $json . PHP_EOL;
+                $info .= $e->getMessage() . PHP_EOL;
+                foreach ($e->getTrace() as $tkey => $tval) {
+                    if (isset($tval['file']) && isset($tval['line'])) {
+                        $info .= $tval['file'] . 'line:' . $tval['line']. PHP_EOL;
+                    }
+                }
+                $info .= 'end==============='.PHP_EOL;
+                echo $info;
                 $message->delivery_info['channel']->basic_cancel($message->delivery_info['consumer_tag']);
             }
         };
